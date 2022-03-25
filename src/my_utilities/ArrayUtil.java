516a6569884d07b6f515ea4e2b9dfc1fd4b2bbb2
anastasia <nastyapetr0388@gmail.com>
2022-03-25 12:53:10 -0400

    updated

--- src/my_utilities/ArrayUtil.java
+++ src/my_utilities/ArrayUtil.java
@@ -1,18 +1,17 @@
 package my_utilities;
 
 public class ArrayUtil {
-
-      /*
-    Min Number
+    /*
+      Min Number
     create a method that will accept an int array and return the smallest number from the array
-     */
 
-    public static int minNumber(int [] nums){
+     */
+    public static int minNumber(int[] nums) {
 
         int min = nums[0];
 
-        for(int num : nums){
-            if(num < min){
+        for (int num : nums) {
+            if (num < min) {
                 min = num;
             }
         }
@@ -24,22 +23,22 @@ public class ArrayUtil {
     create a method that will accept an int array and return the biggest number from the array
      */
 
-    public static int maxNumber(int [] nums){
+    public static int maxNumber(int[] nums) {
 
         int max = nums[0];
 
-        for(int num : nums){
-            if(num > max){
+        for (int num : nums) {
+            if (num > max) {
                 max = num;
+
             }
         }
         return max;
     }
 
-            /*
+    /*
         Contains
-    create a method that will accept an int array and an int number.
-    Check and return if the given number is in the array.
+    create a method that will accept an int array and an int number. Check and return if the given number is in the array.
          */
 
     public static boolean contains(int[] nums, int element) {
@@ -48,11 +47,14 @@ public class ArrayUtil {
 
             if (each == element) {
                 return true;
+
             }
-
         }
-
         return false;
     }
-
 }
+
+
+
+
+
