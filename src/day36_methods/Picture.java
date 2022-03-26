9e751fb6687f1c2867449d211f95acc0036513f0
anastasia <nastyapetr0388@gmail.com>
2022-03-26 10:33:34 -0400

    day36_methods

---
+++ src/day36_methods/Picture.java
@@ -0,0 +1,23 @@
+package day36_methods;
+
+public class Picture {
+
+    public static void draw(){
+        System.out.println("trying to draw");
+    }
+
+    public static void draw(String color){
+        System.out.println("draw with " + color);
+    }
+    public static void draw(String color, String color2){
+        System.out.println("drawing with color " + color + " and with "+ color2);
+    }
+    public static void draw(int size) {
+        System.out.println("drawing with size " + size);
+    }
+ //not valid by just chancging the parametr name
+//    public static void draw(int length){
+//        System.out.println("drawing with size " + length);
+    }
+
+
