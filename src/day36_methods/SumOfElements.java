adf725b2a962a94e9dd4f9e7ea37ad7e97975282
anastasia <nastyapetr0388@gmail.com>
2022-03-26 13:09:14 -0400

    day36_methods

---
+++ src/day36_methods/SumOfElements.java
@@ -0,0 +1,16 @@
+package day36_methods;
+
+public class SumOfElements {
+    public static void main(String[] args) {
+        int [] arr = {1,5,6,2};
+        System.out.println(sum(arr));
+    }
+
+    public static int sum(int [] nums){
+        int sum = 0;
+        for(int each: nums){
+            sum += each;
+        }
+        return sum;
+    }
+}
