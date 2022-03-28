aae9588f3f0ded3860a8225684c984fb3a65e728
anastasia <nastyapetr0388@gmail.com>
2022-03-28 19:36:29 -0400

    upd

---
+++ src/day37_wrapper_arraylist/WrapperClassObject.java
@@ -0,0 +1,18 @@
+package day37_wrapper_arraylist;
+
+public class WrapperClassObject {
+    public static void main(String[] args) {
+
+        int a = 5;
+        Integer a2 = new Integer(10);
+        Integer a3 = 20;
+
+        System.out.println(a);
+        System.out.println(a2);
+        System.out.println(a3);
+
+        byte b = 2;
+        Byte b2 = 20;
+
+    }
+}
