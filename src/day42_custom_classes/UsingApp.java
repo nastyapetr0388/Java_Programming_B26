9398ff4673dafce1903cf5e405372f5bd1c09d95
anastasia <qwerty>
2022-04-10 00:42:38 -0400

    day42 Custom classes

---
+++ src/day42_custom_classes/UsingApp.java
@@ -0,0 +1,19 @@
+package day42_custom_classes;
+
+public class UsingApp {
+    public static void main(String[] args) {
+
+        App cydeo = new App();
+        cydeo.name = "Cydeo";
+        cydeo.version = 5.5;
+        cydeo.isFree = true;
+
+        cydeo.run();
+        cydeo.update();
+
+        System.out.println(cydeo.name);
+        System.out.println(cydeo.version);
+        System.out.println(cydeo.isFree);
+        System.out.println(cydeo);
+    }
+}
