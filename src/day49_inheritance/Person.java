ec092fd69b34195a3d016a23931c11b2de407c27
anastasia <qwerty>
2022-04-19 21:42:16 -0400

    day 49

---
+++ src/day49_inheritance/Person.java
@@ -0,0 +1,23 @@
+package day49_inheritance;
+
+public class Person {
+
+    String name;
+    int age;
+    String favoriteHobby;
+
+    public void talk(){
+        System.out.println(name + " is talking");
+    }
+
+    /*
+    Person is the Super class
+          name, age , favorite hobby
+          talk()
+
+    Student class it the sub class of Person
+           int fun level
+           study()
+
+     */
+}
