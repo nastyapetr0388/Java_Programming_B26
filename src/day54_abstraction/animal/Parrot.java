1e1cfd8cd54cca35e4b409858c6deb001ee7ea84
anastasia <qwerty>
2022-05-03 23:05:05 -0400

    day 54

--- src/day54_abstraction/animal/Parrot.java
+++ src/day54_abstraction/animal/Parrot.java
@@ -1,9 +1,15 @@
 package day54_abstraction.animal;
 
-public final class Parrot extends Bird{
+public final class Parrot extends Bird implements Flyable{
 
     @Override
     public void eat() {
         System.out.println("Parrot woble to the seeds");
     }
+
+    @Override
+    public void fly() {
+        System.out.println("Parrot is flying");
+    }
 }
+
