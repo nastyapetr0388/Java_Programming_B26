848883d6fcd646b06b68934765dd797be2a5b812
anastasia <qwerty>
2022-05-07 11:54:07 -0400

    day 55

---
+++ src/day55_polymorphism/shopping/Runner.java
@@ -0,0 +1,27 @@
+package day55_polymorphism.shopping;
+
+public class Runner {
+    public static void main(String[] args) {
+
+          /*
+        Shopping and OnlineShopping are abstract classes, so we can't make object of them
+        Shipping is an interface, so we can't make an object of it
+         */
+
+        Target target = new Target();
+        target.price = 10;
+        target.buyItem();
+        target.returnItem();
+        //target.payForShipping(true); Target class does not implement the Shipping interface
+
+        Amazon amazon = new Amazon();
+        amazon.price = 1000;
+        amazon.buyItem();
+        amazon.returnItem();
+        amazon.payForShipping(true);
+        amazon.viewCart();
+
+
+    }
+    }
+
---
+++ src/day55_polymorphism/shopping/Target.java
@@ -0,0 +1,18 @@
+package day55_polymorphism.shopping;
+
+    public final class Target extends Shopping{
+
+        @Override
+        public void buyItem() {
+            System.out.println("Buying from Target");
+        }
+
+        @Override
+        public void returnItem() {
+            System.out.println("Returning Item to Target");
+
+        }
+    }
+
+
+
